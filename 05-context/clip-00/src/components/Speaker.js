9f242b0a4ab86c8820e535e9f8077d919b4e7e68
Alex Breskin <alexdbreskin@gmail.com>
2021-08-24 11:06:55 +1000

    🚧 Speaker Filter Context added

--- 05-context/clip-00/src/components/Speaker.js
+++ 05-context/clip-00/src/components/Speaker.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { SpeakerFilterContext } from "../contexts/SpeakerFilterContext";
 
 function Session({ title, room }) {
   return (
@@ -97,8 +98,9 @@ function SpeakerDemographics({
   );
 }
 
-function Speaker({ speaker, showSessions, onFavoriteToggle }) {
+function Speaker({ speaker, onFavoriteToggle }) {
   const { id, first, last, sessions } = speaker;
+  const { showSessions } = useContext(SpeakerFilterContext);
   return (
     <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
       <div className="card card-height p-4 mt-4">
--- 05-context/clip-00/src/components/Speakers.js
+++ 05-context/clip-00/src/components/Speakers.js
@@ -1,18 +1,13 @@
 import SpeakersToolbar from "./SpeakersToolbar";
 import SpeakersList from "./SpeakersList";
-import { useState } from "react";
+import { SpeakerFilterProvider } from "../contexts/SpeakerFilterContext";
 
 function Speakers() {
-  const [showSessions, setShowSessions] = useState(true);
-
   return (
-    <>
-      <SpeakersToolbar
-        showSessions={showSessions}
-        setShowSessions={setShowSessions}
-      />
-      <SpeakersList showSessions={showSessions} />
-    </>
+    <SpeakerFilterProvider startingShowSessions={false}>
+      <SpeakersToolbar />
+      <SpeakersList />
+    </SpeakerFilterProvider>
   );
 }
 
